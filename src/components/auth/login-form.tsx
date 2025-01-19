8fc1daa92498e4b391f2d8f5302f809be2b3cb28
pravin kumar <pravinshanmugam1995@gmail.com>
2025-01-19 20:11:38 +0530

    remove navigate

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -52,9 +52,6 @@ export function LoginForm({
     password,
   }) => {
     await auth.signin(email, password);
-    await navigate({
-      to: "/",
-    });
   };
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
